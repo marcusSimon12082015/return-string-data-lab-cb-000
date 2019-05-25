18e05bfc3d5d6a387d9623e87efd153e568f4e3f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-25 11:40:12 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
 
   def create
-
+    @product = Product.new(product_params)
+    if @product.save 
+      redirect_to products_path(@product)
+    else
+    end 
+    
   end
 
   private
